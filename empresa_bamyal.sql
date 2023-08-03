6d415b05e90db547ea5be88b1d6a4954e77b1e94
chris <chrisfree1122@gmail.com>
2023-08-03 13:36:41 -0500

    base de datos actualizada

--- empresa_bamyal.sql
+++ empresa_bamyal.sql
@@ -16,137 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Categoria`
---
-
-DROP TABLE IF EXISTS `Categoria`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Categoria` (
-  `idCategoria` int NOT NULL,
-  `descripcion` varchar(20) NOT NULL,
-  PRIMARY KEY (`idCategoria`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Categoria`
---
-
-LOCK TABLES `Categoria` WRITE;
-/*!40000 ALTER TABLE `Categoria` DISABLE KEYS */;
-INSERT INTO `Categoria` VALUES (1,'Belleza');
-/*!40000 ALTER TABLE `Categoria` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `DetalleFactura`
---
-
-DROP TABLE IF EXISTS `DetalleFactura`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `DetalleFactura` (
-  `idDetalleFactura` int NOT NULL,
-  `idFactura` int DEFAULT NULL,
-  `idProductos` int DEFAULT NULL,
-  `cantidad` int DEFAULT NULL,
-  `valorTotal` decimal(10,2) NOT NULL,
-  PRIMARY KEY (`idDetalleFactura`),
-  KEY `idFactura` (`idFactura`),
-  KEY `idProductos` (`idProductos`),
-  CONSTRAINT `DetalleFactura_ibfk_1` FOREIGN KEY (`idFactura`) REFERENCES `Factura` (`idFactura`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `DetalleFactura`
---
-
-LOCK TABLES `DetalleFactura` WRITE;
-/*!40000 ALTER TABLE `DetalleFactura` DISABLE KEYS */;
-/*!40000 ALTER TABLE `DetalleFactura` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Estado`
---
-
-DROP TABLE IF EXISTS `Estado`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Estado` (
-  `idEstado` int NOT NULL,
-  `descripcion` varchar(20) NOT NULL,
-  PRIMARY KEY (`idEstado`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Estado`
---
-
-LOCK TABLES `Estado` WRITE;
-/*!40000 ALTER TABLE `Estado` DISABLE KEYS */;
-INSERT INTO `Estado` VALUES (1,'Activo'),(2,'Inactivo');
-/*!40000 ALTER TABLE `Estado` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Factura`
---
-
-DROP TABLE IF EXISTS `Factura`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Factura` (
-  `idFactura` int NOT NULL,
-  `numeroFactura` varchar(20) NOT NULL,
-  `idUsuario` int DEFAULT NULL,
-  `idFormaPago` int DEFAULT NULL,
-  `iva` decimal(10,2) NOT NULL,
-  `totalFactura` decimal(10,2) NOT NULL,
-  PRIMARY KEY (`idFactura`),
-  KEY `idUsuario` (`idUsuario`),
-  KEY `idFormaPago` (`idFormaPago`),
-  CONSTRAINT `Factura_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `Usuario` (`idUsuario`),
-  CONSTRAINT `Factura_ibfk_2` FOREIGN KEY (`idFormaPago`) REFERENCES `FormaPago` (`idFormaPago`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Factura`
---
-
-LOCK TABLES `Factura` WRITE;
-/*!40000 ALTER TABLE `Factura` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Factura` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `FormaPago`
---
-
-DROP TABLE IF EXISTS `FormaPago`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `FormaPago` (
-  `idFormaPago` int NOT NULL,
-  `descripcion` varchar(20) NOT NULL,
-  PRIMARY KEY (`idFormaPago`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `FormaPago`
---
-
-LOCK TABLES `FormaPago` WRITE;
-/*!40000 ALTER TABLE `FormaPago` DISABLE KEYS */;
-/*!40000 ALTER TABLE `FormaPago` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `Productos`
 --
 
@@ -162,7 +31,7 @@ CREATE TABLE `Productos` (
   `fechaElaboracion` date NOT NULL,
   `fechaExpiracion` date NOT NULL,
   PRIMARY KEY (`idProducto`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -171,54 +40,8 @@ CREATE TABLE `Productos` (
 
 LOCK TABLES `Productos` WRITE;
 /*!40000 ALTER TABLE `Productos` DISABLE KEYS */;
+INSERT INTO `Productos` VALUES (8,'Perros','Perro guapo',1,45.45,'2000-02-15','0001-01-01'),(10,'Niurka Murillo','33',56,44.44,'2023-08-18','2023-08-25');
 /*!40000 ALTER TABLE `Productos` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Prueba`
---
-
-DROP TABLE IF EXISTS `Prueba`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Prueba` (
-  `id` int DEFAULT NULL,
-  `nombre` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Prueba`
---
-
-LOCK TABLES `Prueba` WRITE;
-/*!40000 ALTER TABLE `Prueba` DISABLE KEYS */;
-INSERT INTO `Prueba` VALUES (1,'jose');
-/*!40000 ALTER TABLE `Prueba` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Rol`
---
-
-DROP TABLE IF EXISTS `Rol`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Rol` (
-  `idRol` int NOT NULL,
-  `descripcion` varchar(20) NOT NULL,
-  PRIMARY KEY (`idRol`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Rol`
---
-
-LOCK TABLES `Rol` WRITE;
-/*!40000 ALTER TABLE `Rol` DISABLE KEYS */;
-INSERT INTO `Rol` VALUES (1,'Administrador'),(2,'Usuario');
-/*!40000 ALTER TABLE `Rol` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -241,9 +64,7 @@ CREATE TABLE `Usuario` (
   `idEstado` int DEFAULT NULL,
   PRIMARY KEY (`idUsuario`),
   KEY `idRol` (`idRol`),
-  KEY `idEstado` (`idEstado`),
-  CONSTRAINT `Usuario_ibfk_1` FOREIGN KEY (`idRol`) REFERENCES `Rol` (`idRol`),
-  CONSTRAINT `Usuario_ibfk_2` FOREIGN KEY (`idEstado`) REFERENCES `Estado` (`idEstado`)
+  KEY `idEstado` (`idEstado`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -253,7 +74,7 @@ CREATE TABLE `Usuario` (
 
 LOCK TABLES `Usuario` WRITE;
 /*!40000 ALTER TABLE `Usuario` DISABLE KEYS */;
-INSERT INTO `Usuario` VALUES (1,'christopher','freire','09437776989','09559655658','argentinca y pato','chris','12345',1,1);
+INSERT INTO `Usuario` VALUES (1,'christopher','freire','09437776989','09559655658','argentinca y pato','chris','12345',1,1),(2,'Perro','Perro','095550259','0995633526','Trinitaria','Perro','guao',1,1);
 /*!40000 ALTER TABLE `Usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -266,4 +87,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-08-01 19:36:20
+-- Dump completed on 2023-08-03 13:33:44
